09b0656a061ae1fe3340d51c8df93422ff31cba4
ronsavage <ron@savage.net.au>
2016-12-22 16:45:52 +1100

    Patch bug in scripts/export.sh

--- scripts/export.sh
+++ scripts/export.sh
@@ -4,5 +4,5 @@ perl -Ilib scripts/export.all.pages.pl
 perl -Ilib scripts/export.icons.pl
 perl -Ilib scripts/export.layouts.pl
 
-cp html/*.svg html/*.html ~/savage.net.au/Flowers
+cp data/*.svg data/*.html ~/savage.net.au/Flowers
 cp -r ~/savage.net.au/Flowers* $DR/
