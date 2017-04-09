9b79170d3abff85378b43dd037eec9eab7da0aef
ronsavage <ron@savage.net.au>
2017-04-09 11:18:17 +1000

    Fix name of licence in WWW::Garden::Design

--- lib/WWW/Garden/Design.pm
+++ lib/WWW/Garden/Design.pm
@@ -332,7 +332,7 @@ Australian copyright (c) 2014, Ron Savage.
 
 	All Programs of mine are 'OSI Certified Open Source Software';
 	you can redistribute them and/or modify them under the terms of
-	The Artistic License 2.0, a copy of which is available at:
-	http://opensource.org/licenses/alphabetical.
+	The Perl License, a copy of which is available at:
+	http://dev.perl.org/licenses/
 
 =cut
