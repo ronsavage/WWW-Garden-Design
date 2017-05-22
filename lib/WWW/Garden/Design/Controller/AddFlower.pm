49840c33b473b889a7909ae1d6d92c5dbc807857
ronsavage <ron@savage.net.au>
2017-05-22 20:18:24 +1000

    Start determining how to pass attributes to server via Ajax

--- lib/WWW/Garden/Design/Controller/AddFlower.pm
+++ lib/WWW/Garden/Design/Controller/AddFlower.pm
@@ -18,11 +18,12 @@ sub display
 
 	my($item) =
 	{
-		aliases			=> $self -> param('aliases')			|| '-',
-		common_name		=> $self -> param('common_name')		|| '',
-		height			=> $self -> param('height')				|| '-',
-		scientific_name	=> $self -> param('scientific_name')	|| '',
-		width			=> $self -> param('width')				|| '-',
+		aliases				=> $self -> param('aliases')			|| '-',
+		attribute_values	=> $self -> param('attribute_values')	|| '-',
+		common_name			=> $self -> param('common_name')		|| '',
+		height				=> $self -> param('height')				|| '-',
+		scientific_name		=> $self -> param('scientific_name')	|| '',
+		width				=> $self -> param('width')				|| '-',
 	};
 
 	$self -> app -> log -> debug("$_ => $$item{$_}") for sort keys %$item;
