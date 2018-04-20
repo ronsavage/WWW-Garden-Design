ee81d83de56fd07ab9cb1f23ad941c042c94a24d
ronsavage <ron@savage.net.au>
2018-04-20 13:06:48 +1000

    More tweaks. Add some POD to all files

--- lib/WWW/Garden/Design/Util/Filer.pm
+++ lib/WWW/Garden/Design/Util/Filer.pm
@@ -63,26 +63,37 @@ See L<WWW::Garden::Design/Installation>.
 
 Returns a an arrayref of hashref of records.
 
-=head1 FAQ
+=head1 Machine-Readable Change Log
 
-See L<WWW::Garden::Design/FAQ>.
+The file Changes was converted into Changelog.ini by L<Module::Metadata::Changes>.
+
+=head1 Version Numbers
+
+Version numbers < 1.00 represent development versions. From 1.00 up, they are production versions.
+
+=head1 Repository
+
+L<https://github.com/ronsavage/WWW-Garden-Design>
 
 =head1 Support
 
-See L<WWW::Garden::Design/Support>.
+Email the author, or log a bug on RT:
+
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=WWW::Garden::Design>.
 
 =head1 Author
 
-C<WWW::Garden::Design> was written by Ron Savage I<E<lt>ron@savage.net.auE<gt>> in 2017.
+L<WWW::Garden::Design> was written by Ron Savage I<E<lt>ron@savage.net.auE<gt>> in 2014.
 
-L<Home page|https://savage.net.au/index.html>.
+My homepage: L<https://savage.net.au/>.
 
 =head1 Copyright
 
-Australian copyright (c) 2017, Ron Savage.
+Australian copyright (c) 2013, Ron Savage.
+
 	All Programs of mine are 'OSI Certified Open Source Software';
 	you can redistribute them and/or modify them under the terms of
-	The Artistic License V 2, a copy of which is available at:
-	http://www.opensource.org/licenses/index.html
+	The Perl License, a copy of which is available at:
+	http://dev.perl.org/licenses/
 
 =cut
--- lib/WWW/Garden/Design/Util/ValidateForm.pm
+++ lib/WWW/Garden/Design/Util/ValidateForm.pm
@@ -420,3 +420,40 @@ sub process_flower_urls
 # -----------------------------------------------
 
 1;
+
+=pod
+
+=head1 Machine-Readable Change Log
+
+The file Changes was converted into Changelog.ini by L<Module::Metadata::Changes>.
+
+=head1 Version Numbers
+
+Version numbers < 1.00 represent development versions. From 1.00 up, they are production versions.
+
+=head1 Repository
+
+L<https://github.com/ronsavage/WWW-Garden-Design>
+
+=head1 Support
+
+Email the author, or log a bug on RT:
+
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=WWW::Garden::Design>.
+
+=head1 Author
+
+L<WWW::Garden::Design> was written by Ron Savage I<E<lt>ron@savage.net.auE<gt>> in 2014.
+
+My homepage: L<https://savage.net.au/>.
+
+=head1 Copyright
+
+Australian copyright (c) 2013, Ron Savage.
+
+	All Programs of mine are 'OSI Certified Open Source Software';
+	you can redistribute them and/or modify them under the terms of
+	The Perl License, a copy of which is available at:
+	http://dev.perl.org/licenses/
+
+=cut
