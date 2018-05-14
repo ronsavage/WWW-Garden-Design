edbc264307ac1ba8516e83f727d9ae2fa96b4b4a
ronsavage <ron@savage.net.au>
2018-05-14 14:51:26 +1000

    Fix code whereby too much was moved from Initialize to Design. So move it back

--- lib/WWW/Garden/Design.pm
+++ lib/WWW/Garden/Design.pm
@@ -71,40 +71,6 @@ sub build_attribute_type_names
 } # End of build_attribute_type_names.
 
 # -----------------------------------------------
-
-sub build_check_boxes
-{
-	my($self, $type_names, $fields, $ids) = @_;
-
-	my($field);
-	my(@html);
-	my($id_index, $id);
-	my($name_index, $name);
-	my($type_name);
-
-	my(@check_boxes) = map
-						{
-							$name_index	= $_;
-							$type_name	= $$type_names[$name_index];
-							$name		= ucfirst $$type_names[$name_index] =~ s/_/&nbsp;/gr;
-							@html		= map
-											{
-												$id_index	= $_;
-												$field		= $$fields{$type_name}[$id_index] =~ s/_/&nbsp;/gr;
-												$id			= $$ids[$name_index][$id_index];
-
-												"<input id = '$id' type = 'checkbox'>"
-												. "<label for = '$id'>$field</label>";
-											} 0 .. $#{$$ids[$name_index]};
-
-							[$name, join('&nbsp;&nbsp;&nbsp', @html)];
-						} 0 .. $#$type_names;
-
-	return [@check_boxes];
-
-} # End of build_check_boxes.
-
-# -----------------------------------------------
 #	attributes['Native']		= ['No', 'Yes', 'Unknown'];
 #	attributes['Sun tolerance']	= ['Full_sun', 'Part_shade', 'Shade', 'Unknown'];
 
@@ -167,9 +133,6 @@ sub initialize_defaults
 	$$defaults{gardens_garden_menu}			= $$defaults{db} -> build_garden_menu($self, $$defaults{gardens_table}, 'gardens_garden_menu');
 
 	$self -> app -> defaults($defaults);
-	$self -> stash(attribute_check_boxes	=> $self -> build_check_boxes($$defaults{attribute_type_names}, $$defaults{attribute_type_fields}, $$defaults{attribute_attribute_ids}) );
-	$self -> stash(csrf_token				=> $self -> session('csrf_token') );
-	$self -> stash(search_check_boxes		=> $self -> build_check_boxes($$defaults{attribute_type_names}, $$defaults{attribute_type_fields}, $$defaults{search_attribute_ids}) );
 
 	# Find the id of the 1st property on the Properties tab.
 	# This is needed to initialize a JS variable of the same name,
--- lib/WWW/Garden/Design/Controller/Initialize.pm
+++ lib/WWW/Garden/Design/Controller/Initialize.pm
@@ -10,6 +10,40 @@ our $VERSION = '0.96';
 
 # -----------------------------------------------
 
+sub build_check_boxes
+{
+	my($self, $type_names, $fields, $ids) = @_;
+
+	my($field);
+	my(@html);
+	my($id_index, $id);
+	my($name_index, $name);
+	my($type_name);
+
+	my(@check_boxes) = map
+						{
+							$name_index	= $_;
+							$type_name	= $$type_names[$name_index];
+							$name		= ucfirst $$type_names[$name_index] =~ s/_/&nbsp;/gr;
+							@html		= map
+											{
+												$id_index	= $_;
+												$field		= $$fields{$type_name}[$id_index] =~ s/_/&nbsp;/gr;
+												$id			= $$ids[$name_index][$id_index];
+
+												"<input id = '$id' type = 'checkbox'>"
+												. "<label for = '$id'>$field</label>";
+											} 0 .. $#{$$ids[$name_index]};
+
+							[$name, join('&nbsp;&nbsp;&nbsp', @html)];
+						} 0 .. $#$type_names;
+
+	return [@check_boxes];
+
+} # End of build_check_boxes.
+
+# -----------------------------------------------
+
 sub homepage
 {
 	my($self) = @_;
@@ -17,6 +51,10 @@ sub homepage
 	$self -> app -> log -> debug('Initialize.homepage()');
 
 	my($defaults) = $self -> app -> defaults;
+
+	$self -> stash(attribute_check_boxes	=> $self -> build_check_boxes($$defaults{attribute_type_names}, $$defaults{attribute_type_fields}, $$defaults{attribute_attribute_ids}) );
+	$self -> stash(csrf_token				=> $self -> session('csrf_token') );
+	$self -> stash(search_check_boxes		=> $self -> build_check_boxes($$defaults{attribute_type_names}, $$defaults{attribute_type_fields}, $$defaults{search_attribute_ids}) );
 
 	# These parameters are passed to homepage.html.ep for incorporation into JS code.
 
